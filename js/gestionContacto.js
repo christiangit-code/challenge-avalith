48d869cda52aa293f5f539c4663aeff581065b1c
christiansenergia <christiansenergiasoluciones@gmail.com>
2020-02-08 10:34:46 -0430

    funcion agregadr contacto

--- js/gestionContacto.js
+++ js/gestionContacto.js
@@ -1,4 +1,52 @@
-var idRegistroActivo = null; 
+var variableGestionGlobal = {
+
+	idRegistroActivo: null,
+	idSigRegistro: 0,
+
+ 	setIdRegistroActivo: function(idRegistro) 
+ 	{
+    	this.idRegistroActivo = idRegistro;
+  	},
+
+  	getIdRegistroActivo: function() 
+ 	{
+    	return this.idRegistroActivo;
+  	},
+
+	getIdSigRegistro: function() 
+ 	{
+    	return this.idSigRegistro;
+  	},
+
+  	setIdSigtRegistro: function(valor) 
+ 	{
+    	if(typeof(valor) != 'undefined')
+		{
+			this.idSigRegistro = parseInt(valor);
+
+		} else {
+
+			this.idSigRegistro ++;
+		}
+  	},
+};
+
+function Contacto(id, name, username, email, phone, website, zipcode, fechaNacContacto, idPaisContacto)
+{
+
+	this.id = id;
+	this.name = name
+	this.username = username
+	this.email = email;
+	this.fechaNacContacto = fechaNacContacto;
+	this.idPaisContacto = idPaisContacto;
+	this.phone = phone;
+	this.website = website;
+	this.address = {
+
+		zipcode: zipcode,
+	}
+};
 
 
  // Write on keyup event of keyword input element
@@ -18,11 +66,72 @@ $(document).ready(function()
 	});
 });
 
-function validarCampoBlur(id)
+function validarFormulario()
 {
-	// var idObjForm = Array('nombreContacto', 'apellidoContacto', 'mailContacto', 'fechaNacContacto', 'idPaisContacto', 'tlfContacto');
+	var idObjForm = Array('nombreContacto', 'usuarioContacto', 'mailContacto', 'fechaNacContacto', 'idPaisContacto', 'tlfContacto');
+	var retorno = null;
+	var idError = null;
+	var cuerpoTablaBusqueda = document.getElementById('cuerpoTablaContacto');
 
+	var promesaValidar = new Promise(function(resolve, reject) { 
+
+		for (var i = 0; i < idObjForm.length; i++)
+		{
+			resp = validarCampoBlur(idObjForm[i], 'general');
+			if(resp == false)
+			{
+				idError = idObjForm[i];
+				break;
+			}
+		}
+
+		if(resp == true)
+		{
+			resolve(resp);
+
+		} else {
+
+			reject(resp);
+		}
+
+	});
+
+	promesaValidar.then(function(value) {
+
+		var datosRegistrar = Array();
+
+		for (var i = 0; i < idObjForm.length; i++)
+		{
+
+			datosRegistrar[i] = document.getElementById(idObjForm[i]).value;
+		}
+
+		objRegistro = new Contacto(variableGestionGlobal.getIdSigRegistro(),datosRegistrar[0], datosRegistrar[1], 
+			datosRegistrar[2], datosRegistrar[5], 'www.ejemplo.com', '6101', datosRegistrar[3], datosRegistrar[4]);
+
+		addContacto(objRegistro, cuerpoTablaBusqueda);
+		variableGestionGlobal.setIdSigtRegistro();
+
+		toastr.success('Contacto Agregado');
+		document.getElementById('formContacto').reset();
+
+	}).catch(function(value) {
+
+	 	toastr.error('Debe llenar el campo '+document.getElementById(idError).placeholder);
+	});
+
+}
+
+function validarCampoBlur(id, llamado)
+{
+	
+	var retorno = false;
 	var elem = document.getElementById(id);
+
+	if(typeof(llamado) == 'undefined')
+	{
+		var llamado = null;
+	}
 	
 	if(id == 'nombreContacto' || id == 'usuarioContacto' || id== "fechaNacContacto")
 	{
@@ -30,11 +139,11 @@ function validarCampoBlur(id)
 
 		if(valor == null || valor.length == 0 || /^\s+$/.test(valor))
 		{
-			asignarValido(elem, false);
+			retorno = asignarValido(elem, false, llamado);
 
 		} else {
 
-			asignarValido(elem, true);
+			retorno = asignarValido(elem, true, llamado);
 		}
 
 	} else if(id == 'mailContacto')
@@ -43,11 +152,11 @@ function validarCampoBlur(id)
 
 		if(!(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/.test(valor))) 
 		{
-			asignarValido(elem, false);
+			retorno = asignarValido(elem, false, llamado);
 
 		} else {
 
-			asignarValido(elem, true);
+			retorno = asignarValido(elem, true, llamado);
 		}
 
 	} else if(id == 'idPaisContacto')
@@ -56,11 +165,11 @@ function validarCampoBlur(id)
 
 		if(valor == null || valor.length == 0 || /^\s+$/.test(valor))
 		{
-			asignarValido(elem, false);
+			retorno = asignarValido(elem, false, llamado);
 
 		} else {
 
-			asignarValido(elem, true);
+			retorno = asignarValido(elem, true, llamado);
 		}
 
 	} else if(id == 'tlfContacto')
@@ -69,27 +178,48 @@ function validarCampoBlur(id)
 
 		if(valor == null || valor.length == 0 || /^\s+$/.test(valor))
 		{
-			asignarValido(elem, false);
+			asignarValido(elem, false, llamado);
 
 		} else {
 
-			asignarValido(elem, true);
+			retorno = asignarValido(elem, true, llamado);
 		}
 	}
 
+	return retorno;
+
 }
 
-function asignarValido(obj, valor)
+function asignarValido(obj, valor, llamado)
 {
+	var retorno = false;
+
 	if(valor == true)
 	{
-		obj.style.backgroundColor = "";
+		if(llamado != null)
+		{
+			retorno = true;
+
+		} else {
+
+			obj.style.backgroundColor = "";
+		}
 
 	} else {
 
-		obj.style.backgroundColor = "#FFFFCC";
-		toastr.warning("*Campo Obligatorio ("+obj.placeholder+")");
+		if(llamado != null)
+		{
+			retorno = false;
+			
+		} else {
+
+			obj.style.backgroundColor = "#FFFFCC";
+			toastr.warning("*Campo Obligatorio ("+obj.placeholder+")");
+		}
+	
 	}
+
+	return retorno;
 }
 
 function armarTabla()
@@ -99,23 +229,30 @@ function armarTabla()
 
 	var registros = JSON.parse(this.responseText);
 
+	variableGestionGlobal.setIdSigtRegistro(registros.length+1);
+
 	for (var i = 0; i < registros.length; i++)
     {
-    	fila = crearObjeto('tr',{'id':'contacto'+registros[i].id,},null,null);
 
-		columnaNombre = crearObjeto('td',{},registros[i].name,fila);
-		columnaUsuario = crearObjeto('td',{},registros[i].username,fila);
-
-		columnaMail = crearObjeto('td',{},registros[i].email,fila);
-		columnaSitioWeb = crearObjeto('td',{},registros[i].website,fila);
-		columnaCodigoPostal = crearObjeto('td',{},registros[i].address.zipcode,fila);
-		columnaTlf = crearObjeto('td',{},registros[i].phone,fila);
-
-		columnaEliminar = crearObjeto('button', {'id': 'botonEliminar'+registros[i].id, 'onClick': 'asignarRegistroEliminar(\'contacto'+registros[i].id+'\')', 'type': 'button', 'class': 'btn btn-outline-dark', 'data-toggle':"modal", 'data-target': "#exampleModal"}, 'Eliminar', fila);
-		
-		asociaHijo(fila, cuerpoTablaBusqueda);
+    	addContacto(registros[i], cuerpoTablaBusqueda);
 		
     }
+}
+
+function addContacto(objRegistro, cuerpoTablaBusqueda)
+{
+	fila = crearObjeto('tr',{'id':'contacto'+objRegistro.id}, null, null);
+
+	columnaNombre = crearObjeto('td', {}, objRegistro.name, fila);
+	columnaUsuario = crearObjeto('td', {}, objRegistro.username, fila);
+	columnaMail = crearObjeto('td', {}, objRegistro.email, fila);
+	columnaSitioWeb = crearObjeto('td', {}, objRegistro.website, fila);
+	columnaCodigoPostal = crearObjeto('td', {}, objRegistro.address.zipcode, fila);
+	columnaTlf = crearObjeto('td', {}, objRegistro.phone,fila);
+
+	columnaEliminar = crearObjeto('button', {'id': 'botonEliminar'+objRegistro.id, 'onClick': 'asignarRegistroEliminar(\'contacto'+objRegistro.id+'\')', 'type': 'button', 'class': 'btn btn-outline-dark', 'data-toggle':"modal", 'data-target': "#exampleModal"}, 'Eliminar', fila);
+	
+	asociaHijo(fila, cuerpoTablaBusqueda);
 }
 
 function consultarConatcto()
@@ -126,7 +263,6 @@ function consultarConatcto()
 	oReq.send();
 
 }
-
 
 function armarComboPais()
 {
@@ -149,22 +285,20 @@ function consultarPais()
 	oReq.send();
 
 }
-/*
-function eliminarContacto(idFila)
-{
-	eliminaHijo(document.getElementById(idFila));
-}*/
 
 function asignarRegistroEliminar(idFila)
 {
-	idRegistroActivo = idFila;
+
+	variableGestionGlobal.setIdRegistroActivo(idFila);
 }
 
 function verificaRegistroAEliminar()
 {
-	if(idRegistroActivo != null)
+	if(variableGestionGlobal.getIdRegistroActivo() != null)
 	{
-		eliminaHijo(document.getElementById(idRegistroActivo));
+		
+		eliminaHijo(document.getElementById(variableGestionGlobal.getIdRegistroActivo()));
+		
 	}
 }
 //-------------------
@@ -188,7 +322,6 @@ function crearObjeto(tipo,propiedades,texto,nodoPadre)
 	if(nodoPadre != null)
 	{
 		xAppendChild(nodoPadre,objHtml);
-		//return;
 		
 	} else {
 		
@@ -213,35 +346,6 @@ function eliminaHijo(objHtml)
 	
 }
 
-function insertarTexto(objHtml,texto)
-{
-	var objTtexto = document.createTextNode(texto);
-	xAppendChild(objHtml,objTtexto);
-
-	
-}
-
-/*function agregarAtributo(objHtml,propiedades)
-{
-	 if (!xDef(objHtml) || !xDef(propiedades) || (typeof propiedades != 'object')) return;
-
-        for (pr in propiedades)
-        {
-			objHtml.setAttribute(pr, propiedades[pr]);
-            
-        }
-}*/
-
-/*function eliminarNodo(id)
-{
-	var nodoEliminar = xGetElementById(id);
-	
-	if(nodoEliminar != null)
-	{
-		nodoEliminar.parentNode.removeChild(nodoEliminar);
-	}
-}
-*/
 function xCreateElement(sTag)
 {
   if (document.createElement) return document.createElement(sTag);
